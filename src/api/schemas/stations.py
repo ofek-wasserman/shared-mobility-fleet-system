105d68cc2ed93098f47944ec34ffd7258a5609e2
Eden Yarkoni <edenyarkoni@gmail.com>
2026-02-25 22:52:29 +0200

    fixed naming issues

--- src/api/schemas/stations.py
+++ src/api/schemas/stations.py
@@ -1,11 +1,9 @@
 from src.api.schemas.base import StrictBaseModel
 
-
 class NearestStationResponse(StrictBaseModel):
     station_id: int
     lat: float
     lon: float
-
-
-class ActiveUserResponse(StrictBaseModel):
+    
+class ActiveUsersResponse(StrictBaseModel):
     user_id: list[int]
